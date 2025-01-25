9b0dff870b56fd09566c06486b9ab9b89faf6c39
Eddie <edkwak44@gmail.com>
2025-01-25 00:07:03 -0500

    First commit.

--- isi_robot.py
+++ isi_robot.py
@@ -1,10 +1,18 @@
 class Robot:
     def __init__(self, num_subsystems, num_fans, fan_max_speed):
+        """Initializes a new Robot.
+
+        Args:
+            num_subsystems (int): Number of subsystems
+            num_fans (int): Number of fans
+            fan_max_speed (int): The maximum fan speed
+        """
         self.num_subsystems = num_subsystems
         self.num_fans = num_fans
         self.fan_max_speed = fan_max_speed
         self.subsystem_temperatures = [20.0] * num_subsystems
-        
+    
+    
     def __str__(self):
         """Equivalent to Java's toString - Returns string representation of a Robot object.
 
